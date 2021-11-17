74f8e3d3a30e5f01afa1b946e7bae4542118c8ad
Allysa Yap <allysayap200010@gmail.com>
2021-11-17 15:19:27 -0500

    update

--- ToMeet/AppDelegate.swift
+++ ToMeet/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "erOZWjvWmWoQkB2M0Nd2hsy5GTVshrsUyfOasWvn" // <- UPDATE
+                    $0.clientKey = "DcE7WxCQl68mSFcfhmB23faY3gsAb395jz7IG4Fu" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         
         return true
     }
